d0bf69c59a075399eef42317b0cd75effd97f25c
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2015-08-06 08:12:13 +0200

    fixed osx deployment

---
+++ deploy_osx.sh
@@ -0,0 +1,9 @@
+#~/Qt/5.4/clang_64/bin/macdeployqt bin/PointOfSale.app -dmg -verbose=3
+#~/Qt/5.5/clang_64/bin/macdeployqt \
+#/Users/thomashoffmann/Documents/Projects/qt/PointOfSale/bin/PointOfSale.app \
+#-verbose=3 \
+#-qmldir=/Users/thomashoffmann/Documents/Projects/qt/PointOfSale/pointofsale/qml
+
+~/Qt/5.5/clang_64/bin/macdeployqt bin/PointOfSale.app -qmldir=/Users/thomashoffmann/Documents/Projects/qt/PointOfSale/pointofsale/qml
+cp -r ~/Qt/5.5/clang_64/qml bin/PointOfSale.app/Contents/Resources/
+find bin/PointOfSale.app/Contents/Resources/qml -name "*_debug*" -exec rm -rf {} \;
